d03634a2ee74302520cb0f5172952ac04b241e03
Kapusbenjamin <bkap100@gmail.com>
2023-04-04 16:42:10 +0200

    360 proba

--- react_photo_viewer/src/App.js
+++ react_photo_viewer/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ThreeSixty from "react-360-view";
 
-function App() {
+const basePath ="http://localhost:3000/react_photo_viewer/src/360.jpeg";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThreeSixty
+        amount={1}
+        imagePath={basePath}
+        spinReverse
+      />
     </div>
   );
 }
-
-export default App;
